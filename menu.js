c0d07accbe7bb2fea95ca20f3128476957a1fb5a
abdallah <abddoullah.bouzhar@gmail.com>
2022-11-15 14:44:05 +0100

    ok

--- menu.js
+++ menu.js
@@ -1,3 +1,8 @@
+window.addEventListener('DOMContentLoaded',function(){
+document.querySelector(".ul").classList.remove("hide");
+
+});
+
 let menuBar = document.getElementById("fa-bars");
 menuBar.addEventListener('click', function(){
     let menu= document.querySelector(".ul");
